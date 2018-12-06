037586724d958eddf1914b40fd28deb87e2d3ccb
unknown <celi.ho@gmail.com>
2018-12-06 06:29:36 -0500

    update server URL

--- client/src/constants/server.js
+++ client/src/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'https://localhost:8081'
+  ? 'http://localhost:8081'
   : 'http://localhost:8081';
 
 export default PAYMENT_SERVER_URL;
