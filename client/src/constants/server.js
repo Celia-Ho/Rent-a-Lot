3494b591d769a8362b66524fbf957925929d60fd
unknown <celi.ho@gmail.com>
2018-12-06 12:43:59 -0500

    updat localhost port

--- client/src/constants/server.js
+++ client/src/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'http://localhost:8000'
-  : 'http://localhost:8000';
+  ? 'https://localhost:8000'
+  : 'https://localhost:8000';
 
 export default PAYMENT_SERVER_URL;
