bf9e34a53e6eec23f4bf8a18b57d48404363d3b2
unknown <celi.ho@gmail.com>
2018-12-06 05:52:54 -0500

    production build

--- client/src/constants/stripe.js
+++ client/src/constants/stripe.js
@@ -1,5 +1,5 @@
 const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_live_MY_PUBLISHABLE_KEY'
+  ? 'pk_test_6AR4PEHWz2hpWCEdGGUWc5oA'
   : 'pk_test_6AR4PEHWz2hpWCEdGGUWc5oA';
 
 export default STRIPE_PUBLISHABLE;
